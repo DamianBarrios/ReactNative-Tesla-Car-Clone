e431e7df6505de898cc471f1da51266c3ddb780d
Damian Barrios <barrios.d122@gmail.com>
2021-03-29 21:07:05 -0300

    rendering first content

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <View>
+          <Text>Model S</Text>
+          <Text>Starting at $69,420</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,4 +22,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+
+  }
 });
